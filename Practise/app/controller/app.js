4c526ea749789e6dacac0307a4279add07b14e52
Ameya Kale <ameyakale24@gmail.com>
2022-05-01 16:22:18 +0530

    Angular JS from start

--- Practise/app/controller/app.js
+++
@@ -1,8 +0,0 @@
-var myapp=angular.module('myapp',[]);
-
-myapp.controller('myappController',function($scope){
-
-    $scope.message="This message is from controller";
-    $scope.array=['Kakashi','Gojo','Sukuna'];
-    
-});
